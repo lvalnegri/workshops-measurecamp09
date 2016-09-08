0d27dd70a999d50319e7a2930270ded6755d26fc
Luca <l.valnegri@datamaps.co.uk>
2016-09-08 14:02:23 +0100

    Create packages.R

---
+++ packages.R
@@ -0,0 +1,9 @@
+# Quick way to install all packages needed for the demo (R code)
+
+dep.pkg <- c('devtools', 'data.table', 'dplyr', 'DT', 'dygraphs',  'forecast', 'funModeling', 'geojsonio', 'ggmap', 'ggplot2', 'ggvis', 
+             'htmlwidgets', 'jsonlite', 'leaflet', 'lubridate', 'maptools', 'rgdal', 'RMySQL')
+pkgs.not.installed <- dep.pkg[!sapply(dep.pkg, function(p) require(p, character.only = TRUE))]
+if( length(pkgs.not.installed) > 0 ) install.packages(pkgs.not.installed, dependencies = TRUE)
+
+
+# How to create a  copy your hard job done in Rstudio (Linux code)
