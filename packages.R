dc60b773764325c6d2475b8d3e6c3312ad859df5
Luca <l.valnegri@datamaps.co.uk>
2016-09-08 17:29:24 +0100

    Update packages.R

--- packages.R
+++ packages.R
@@ -2,6 +2,7 @@
 
 dep.pkg <- c('devtools', 'data.table', 'dplyr', 'DT', 'dygraphs',  'forecast', 'funModeling', 'geojsonio', 'ggmap', 'ggplot2', 'ggvis', 
              'htmlwidgets', 'jsonlite', 'leaflet', 'lubridate', 'maptools', 'rgdal', 'RMySQL')
+# dep.pkg <- c('devtools', 'data.table', 'DT', 'ggplot2', 'jsonlite', 'leaflet')
 pkgs.not.installed <- dep.pkg[!sapply(dep.pkg, function(p) require(p, character.only = TRUE))]
 if( length(pkgs.not.installed) > 0 ) install.packages(pkgs.not.installed, dependencies = TRUE)
 
